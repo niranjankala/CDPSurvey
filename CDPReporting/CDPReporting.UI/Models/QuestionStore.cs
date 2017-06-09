2a7b6721ef31172fed52f0d299e899c361d972fa
Pintu Kumar <pintu.kumar@mind-infotech.com>
2017-06-09 20:17:53 +0530

    QuestionStore class added

---
+++ CDPReporting/CDPReporting.UI/Models/QuestionStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CDPReporting.UI.Models
+{
+    public class QuestionStore
+    {
+        private static volatile QuestionStore instance;
+        private static object syncRoot = new Object();
+
+        private QuestionStore() {
+            questions = new List<Question>();
+        }
+
+        public static QuestionStore Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new QuestionStore();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        List<Question> questions;
+
+        public List<Question> Questions
+        {
+            get { return questions; }
+        }
+
+    }
+}
