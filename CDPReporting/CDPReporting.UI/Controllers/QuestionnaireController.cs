8bfd050ace2d5aeaa2981ce5074dce428d1a2f3a
Niranjan Kala <niranjan.kala@gmail.com>
2017-06-10 16:17:27 +0530

    done changes in classs..

--- CDPReporting/CDPReporting.UI/Controllers/QuestionnaireController.cs
+++ CDPReporting/CDPReporting.UI/Controllers/QuestionnaireController.cs
@@ -14,18 +14,11 @@ namespace CDPReporting.UI.Controllers
 
         public ActionResult Index()
         {           
-            QuestionGroup group1 = new QuestionGroup();
-            group1.QuestionGroupId = "CC0";
-            group1.QuestionGroupName = "CC0. Introduction page";
-            group1.Questions.Add(new Question { QuestionId = "CC01", QuestionGroupId = "CC0",  Type = QuestionType.Simple, QuestionText = "CC0.1. Please give a general description and introduction to your organization [maximum 5000 characters]."});
-            group1.Questions.Add(new Question { QuestionId = "CC02", QuestionGroupId = "CC0", Type = QuestionType.DateRange, QuestionText = "CC0.2. Please state the start and end date of the year for which you are reporting data."});
-            group1.Questions.Add(new Question { QuestionId = "CC03", QuestionGroupId = "CC0", Type = QuestionType.Option, QuestionText = "CC0.3. Please select the currency in which you would like to submit your response. All financial information contained in the response."});
-            group1.Questions.Add(new Question { QuestionId = "CC04", QuestionGroupId = "CC0", Type = QuestionType.Option, QuestionText = "CC0.4: 	Please select if you wish to complete a shorter information request [SME questionnaire only]" });
             
-            //var model = new QuestionModelResult();
-            //model.QuestionGroupList = group1;
+            var model = new QuestionModelResult();
+            model.QuestionGroupList = QuestionStore.Instance.QuestionGroups.ToList();
                                                 
-            return View(group1);
+            return View(model);
         }
 	}
 }
--- CDPReporting/CDPReporting.UI/Models/Question.cs
+++ CDPReporting/CDPReporting.UI/Models/Question.cs
@@ -6,17 +6,57 @@ using System.Web;
 namespace CDPReporting.UI.Models
 {
     public class Question
-    {
+    {   
+        public Question(QuestionType questionType)
+        {
+            type = questionType;
+            InitializeQuestion();
+        }
+        private List<string> options;
+
+        public List<string> Options
+        {
+            get { return options; }
+            set { options = value; }
+        }
+
         public string QuestionId { get; set; }
         public string QuestionGroupId { get; set; }
-        public QuestionType Type { get; set; }
+        private QuestionType type;
+
+        public QuestionType Type
+        {
+            get { return type; }
+            private set { type = value; }
+        }
+
         public string QuestionText { get; set; }
+        void InitializeQuestion()
+        {
+            switch (type)
+            {
+                case QuestionType.Simple:
+                case QuestionType.Boolean:
+                case QuestionType.Date:
+                case QuestionType.DateRange:
+                case QuestionType.Option:
+                case QuestionType.OptionList:
+                case QuestionType.CDPGrid:
+                    break;
+            }
+        }
+
+
     }
 
     public enum QuestionType
     {
-        Simple = 0,
-        Option = 1,
-        DateRange = 2
+        Simple,
+        Option,
+        OptionList,
+        DateRange,
+        Date,
+        Boolean,
+        CDPGrid
     }
 }
