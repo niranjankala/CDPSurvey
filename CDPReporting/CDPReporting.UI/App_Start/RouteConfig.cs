c66b2629c36416c03879c14171d5ba653921549b
mrigesh.gupta <mrigesh.gupta@gmail.com>
2017-06-10 16:20:54 +0530

    changes

--- CDPReporting/CDPReporting.UI/App_Start/RouteConfig.cs
+++ CDPReporting/CDPReporting.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CDPReporting.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Questionnaire", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
