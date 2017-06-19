83485ec5e3b8b15b4ee4f05e7d9c575560a7c6c3
Pintu Kumar <pintu.kumar@mind-infotech.com>
2017-06-19 12:32:15 +0530

    lib

---
+++ CDPReporting/CDPReporting.UI/Controller/AcntController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CDPReporting.UI.Controller
+{
+    public class AcntController : Controller
+    {
+        //
+        // GET: /Acnt/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
