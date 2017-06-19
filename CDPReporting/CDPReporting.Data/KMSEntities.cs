7b52b518eb52a68cce258e33d4348e594c8719bd
Pintu Kumar <pintu.kumar@mind-infotech.com>
2017-06-19 16:40:09 +0530

    -

---
+++ CDPReporting/CDPReporting.Data/KMSEntities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDPReporting.Data
+{
+    public class KMSEntities : SMR.KM.Data.Entities.KMEntities
+    {
+    }
+}
