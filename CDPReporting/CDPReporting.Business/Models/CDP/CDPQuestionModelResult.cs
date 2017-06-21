57484ffba609b43051779a7f96d1e723df1c983e
niranjankala <niranjankala@aol.com>
2017-06-21 09:00:45 +0530

    -

---
+++ CDPReporting/CDPReporting.Business/Models/CDP/CDPQuestionModelResult.cs
@@ -0,0 +1,13 @@
+﻿using CDPReporting.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CDPReporting.Business.Models
+{
+    public class CDPQuestionModelResult
+    {
+        public List<CDPQuestionGroup> QuestionGroupList { get; set; }
+    }
+}
---
+++ CDPReporting/CDPReporting.UI/Controllers/CDPController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CDPReporting.UI.Models;
+using CDPReporting.Business.Models;
+
+namespace CDPReporting.UI.Controllers
+{
+    public class CDPController : Controller
+    {
+        //
+        // GET: /Questionnaire/                
+
+        public ActionResult Index()
+        {           
+            
+            var model = new CDPQuestionModelResult();
+            model.QuestionGroupList = QuestionStore.Instance.QuestionGroups.ToList();
+                                                
+            return View(model);
+        }
+	}
+}
