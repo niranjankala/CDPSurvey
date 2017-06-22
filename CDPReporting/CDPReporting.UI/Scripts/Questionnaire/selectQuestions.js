868dca8232d0f9de1d9987706e63a7b52bb1aedb
niranjankala <niranjankala@aol.com>
2017-06-22 20:41:56 +0530

    changed tables and struture

--- CDPReporting/CDPReporting.UI/Scripts/Questionnaire/selectQuestions.js
+++ CDPReporting/CDPReporting.UI/Scripts/Questionnaire/selectQuestions.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     // Hide all Question Response
     var selectedDivId;
-    $("#questionListDiv>div>div>div>div").click(function () {
+    $("#questionListDiv>div>div>div").click(function () {
         debugger;
         selectedDivId = (this.id).replace(".", "_");
         $('#questionResponseDiv').load(questionViewURL + "?questionViewId=" + selectedDivId);
